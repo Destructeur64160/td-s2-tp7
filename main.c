ec1ec1aa732486dd2b37dfb990424313aff52f8e
Cooper <kevinjallet64@gmail.com>
2026-04-01 15:45:17 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,33 @@
 #include "fonctions.h"
 #include "trislents.h"
 #include "trisrapides.h"
-#define TAILLE 10
+#define TAILLE 100
 
 int main(){
-    printf("C'est le TP7 !");
+    int* tab;
+    int* tab1;
+    int* tab2;
+
+    tab=allouer(TAILLE);
+    tab1=allouer(TAILLE);
+    tab2=allouer(TAILLE);
+
+    initialiser(tab, TAILLE);
+    copie(tab, tab1, TAILLE);
+    copie(tab, tab2, TAILLE);
+
+    afficher(tab, TAILLE);
+    printf("\n");
+
+    printf("tri rapide \n");
+    trirapide(tab1, 0, TAILLE-1);
+    afficher(tab1, TAILLE);
+    printf("\n");
+
+    printf("tri fusion \n");
+    trifusion(tab2, 0, TAILLE-1);
+    afficher(tab2, TAILLE);
+    printf("\n");
+
     return 0;
 }
